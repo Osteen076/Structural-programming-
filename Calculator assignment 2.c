b468bc4ecd355f2c14a63b60d2582165cd7ac180
Osteen076 <159956375+Osteen076@users.noreply.github.com>
2024-02-16 13:10:19 +0300

    Update Calculator assignment 2.c

--- Calculator assignment 2.c
+++ Calculator assignment 2.c
@@ -1,1 +1,26 @@
-#include <stdio.h>int main() {	float num1, num2;	float sum, difference, product, quotient;		printf("Enter the first number: ");	scanf("%f", &num1);	printf("Enter the second number: ");	scanf("%f", &num2);		sum = num1 + num2;	difference = num1 - num2;	quotient = num1 / num2;	product = num1 * num2;		printf("The sum of %.2f and %.2f is: %.2f\n", num1, num2, sum);	printf("The difference of %.2f and %.2f is: %.2f\n", num1, num2, difference);	printf("The sum and difference are: %.2f and %.2f\n", sum, difference);	printf("The product of %.2f and %.2f is: %.2f\n", num1, num2, product);	printf("The quotient of %.2f and %.2f is %.2f\n", num1, num2, quotient);		return 0;}
+#include <stdio.h>
+
+int main() {	
+
+
+float num1, num2;	
+float sum, difference, product, quotient;		
+
+printf("Enter the first number: ");	
+scanf("%f", &num1);	
+printf("Enter the second number: ");
+scanf("%f", &num2);		
+
+sum = num1 + num2;
+difference = num1 - num2;
+quotient = num1 / num2;
+product = num1 * num2;		
+
+printf("The sum of %.2f and %.2f is: %.2f\n", num1, num2, sum);	
+printf("The difference of %.2f and %.2f is: %.2f\n", num1, num2, difference);	
+printf("The sum and difference are: %.2f and %.2f\n", sum, difference);	
+printf("The product of %.2f and %.2f is: %.2f\n", num1, num2, product);	
+printf("The quotient of %.2f and %.2f is %.2f\n", num1, num2, quotient);		
+
+return 0;
+}
