ae58cd8da8551e0b4c948e7fcf6731b982217736
Osteen076 <159956375+Osteen076@users.noreply.github.com>
2024-03-05 14:19:51 +0300

    Add files via upload

---
+++ matrix.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define row 3;
+#define column 3;
+int matrix[row][column] = {{1,2,3},{4,5,6}{7,8,9};
+int main() {
+	int transpose[3][3];
+	for(int i = 0;i<3; i++) {
+		for(int j = 0; j<3; j++) {
+			transpose[j][i] = matrix[i][j];
+}
+}	}
+
+
+
+
+
+
+ 
